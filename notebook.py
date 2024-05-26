d62b6c2e9c624105a79fd5b357a28b401a9b77a1
Roshini Singh <roshini.singh2402@gmail.com>
2024-05-26 14:07:41 +0530

    updated

--- notebook.py
+++ notebook.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn import metrics
+
 
 def function_pred():
     plantDepth = pd.read_csv("tomato_details.csv")
